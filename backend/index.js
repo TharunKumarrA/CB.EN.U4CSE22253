cca22c84fb81538a69339d0d9c7ec3dbd0e6f3fd
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-05-12 20:20:47 +0530

    added cors and dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import stockRoutes from "./routes/stockRoutes.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -13,5 +19,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
