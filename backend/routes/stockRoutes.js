b81c1981715346192904b364e085c50605dbdcc6
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-05-12 19:52:43 +0530

    added stock corelation route

--- backend/routes/stockRoutes.js
+++ backend/routes/stockRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAverageStockPrice } = require("../controllers/stockController");
+const {
+  getAverageStockPrice,
+  getStockCorrelation,
+} = require("../controllers/stockController");
 
 router.get("/stocks/:ticker", getAverageStockPrice);
+
+router.get("/stocks/correlation", getStockCorrelation);
 
 module.exports = router;
